169a4ec364ea09f67bfb2774b6a62b6c61c456b5
whyrot <fuckclankers@tutamail.com>
2025-08-12 01:45:55 -0400

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -2,20 +2,22 @@ import random
 import string
 
 
-def generate_random_string(length):
+def passgen(length):
     characters = string.digits + string.punctuation + string.ascii_letters
     return ''.join(random.choices(characters, k=length))
 
 ask = input("would you like to generate a secure password? y/n: ")
 if ask == "y":
     length = int(input("Enter the length of the random string: "))
-    random_string = generate_random_string(length)
+    password = passgen(length)
 
+    username = input("enter the username/email here: ")
+    
+    final = (f"{username}:{password}\n")
+    print(f"{username}:{password}")
+    with open("passwords.txt", "a") as file:
+        file.write(final)
 
-    print(f"Generated Random String: {random_string}")
-    with open("passwords.txt", "w") as file:
-        file.write(random_string)
-
-    print("The random string has been saved to passwords.txt")
+    print("The password and username combo has been saved to passwords.txt")
 else:
     exit()
